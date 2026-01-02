43d4992a8a226558621b100a55cb4a417e9bff34
Sambhav <sambhav12wakhariya@gmail.com>
2026-01-02 23:54:43 +0530

    Loops in Python For Loop Code Updated

---
+++ Module03/class15.py
@@ -0,0 +1,61 @@
+# LOOP in Python
+
+# FOR LOOP
+
+# Re usable piece of code
+
+# Range Function
+
+# Syntax : 
+# range( start, stop, steps )
+# 1 start default 0
+# 6 stop
+# 1 default
+
+# 1 2 3 4 5
+
+# name = "Sambhav"
+
+# print(f"1.{name}")
+# print("2.Sambhav")
+# print("3.Sambhav")
+# print("4.Sambhav")
+# print("5.Sambhav")
+# print("6.Sambhav")
+# print("7.Sambhav")
+# print("8.Sambhav")
+# print("9.Sambhav")
+# print("10.Sambhav")
+
+# for index in range(1, 11, 1) :
+#     print(index)
+
+# for i in range(1, 11) : 
+#     print(f"{i} Sambhav")
+
+# LOOP for String FOR LOOP
+
+# We can use for loop in 2 types on String
+
+# name = input("Please enter your name : ")
+
+# # We can use index values to print this name
+
+# for index in range(len(name)):
+#     print(name[index])
+
+# Method 02
+
+# name = input("Please enter your name : ")
+
+# for i in name : 
+#     print(i)
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6])
+
