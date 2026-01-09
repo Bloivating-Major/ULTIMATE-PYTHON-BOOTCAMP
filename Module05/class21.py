1ded5987172dcf2280bffaa2e2362600c9a4fb94
Sambhav <sambhav12wakhariya@gmail.com>
2026-01-09 23:25:53 +0530

    Class 21 Notes Updated

---
+++ Module05/class21.py
@@ -0,0 +1,97 @@
+# LIST 
+# A list is a collection that can store
+# multiple values in a single variable
+# a = 1
+# b = 2
+# c = 3
+# stu01 = "Tanuja                               "  
+# stu02 = "Misba"
+# stu03 = "Samiksha"
+# stu04 = "Vaishnavi"
+
+# names = ["Tanuja", "Misba", "Samiksha", "Vaishnavi"]
+# ages = [89, 98, 79, 97]
+
+# List are heterogeneous
+# Heterogeneous => Different vegle
+# Homogeneous => Ek sarke
+
+# sampleData = ["Tanuja", 89, print("I am Hero"), True]
+# print(sampleData)
+
+# Duplicates are also allowed
+# names = ["Tanuja", "Tanuja", 89, 89]
+# print(names)
+
+# Lists are Mutable / Immutable
+# Strings are Immutable
+# name = "Tanuja"
+# print(name[0])
+# name[0] = "K"
+# print(name) 
+# Lists are Mutable
+# age = [1, 2, 3, 4, 5]
+# print(age[0], age[1])
+# name = [ "Tanuja" ]
+# print(name[0])
+# name[0] = "Kanuja"
+# print(name)
+
+# Indexing in List
+# numberList = [1, 2, 3, 4, 5, 6, 7, 8]
+# print(numberList[3])
+# print(numberList[-4])
+# print(numberList[3 : 6 : 1])
+
+# a = [10 , 20 , 30, 45, 50]
+# print(a)
+# a[3] = 40
+# print(a)
+
+# COPY 
+
+# age01 = [ 50 , 100]
+# print(age01)
+# age02 = age01
+# print(age02)
+# age02[0] = 150
+# print(age02)
+# print(age01)
+
+
+# name01 = "Tanuja"
+# # print(name01)
+# name02 = name01
+# # print(name02)
+# name02 = "Misba"
+
+# print(name02)
+# print(name01)
+
+# Pass By Value with Strings
+
+# Pass by Reference Lists
+
+# a = [10, 20, 30]
+
+# b = a.copy()
+
+# b[0] = 100
+
+# print(b)
+# print(a)
+
+# a = [10, 20, 30, 40, 50, 60]
+# for i in a :
+#     print(i)
+
+# a = [10, 20, 30, 40, 50, 60]
+# for i in range(len(a)):
+#     print(a[i])
+
+# a = [5, 3, 2, 7, 6, 45, 34, 21, 99, 87]
+# print(a)
+# a.sort()
+# print(a)
+
+
